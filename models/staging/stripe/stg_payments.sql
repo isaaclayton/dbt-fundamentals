242182127da12dc818160766bd6ee2b53b69069c
Isaac Layton <isaacdlayton@gmail.com>
2022-05-05 21:56:50 +0000

    status of repo after sources lesson

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,8 @@
-with payments as (
-    select
-      id as payment_id
-    , orderid as order_id
-    , paymentmethod as payment_method
-    , status
-    , amount/100 as amount
-    , created as created_at
-    from dbt-tutorial.stripe.payment
-)
-select * from payments
+select
+    id as payment_id
+  , orderid as order_id
+  , paymentmethod as payment_method
+  , status
+  , amount/100 as amount
+  , created as created_at
+from {{ source('stripe', 'payment') }}
